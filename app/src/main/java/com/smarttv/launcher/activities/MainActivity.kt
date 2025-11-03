6cf6a49aa7ddea339bfd28a05943f0447914e3f0
AmirCode97 <amirhoseinsalehifashami777@gmail.com>
2025-11-03 20:56:08 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/smarttv/launcher/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.smarttv.launcher.activities
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.smarttv.launcher.R
+import com.smarttv.launcher.fragments.MainFragment
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_browse_fragment, MainFragment())
+                .commitNow()
+        }
+    }
+}
